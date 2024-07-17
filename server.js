a16c8b61fea2303c10346ac5f52ccdcdac0a2c53
kali_wsl <drgsl@outlook.com>
2024-07-17 16:36:54 +0300

    test

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.get('/run-cpp', (req, res) => {
+    exec('./script', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing script: ${error}`);
+            return res.status(500).send(`Error: ${error.message}`);
+        }
+        if (stderr) {
+            console.error(`Script stderr: ${stderr}`);
+            return res.status(500).send(`Error: ${stderr}`);
+        }
+        res.send(stdout);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
